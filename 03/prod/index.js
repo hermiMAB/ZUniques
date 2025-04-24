0fb35a8d1c6baeb974077af8b3ffbadcf4058969
Shalom-Ali <m80009119@zu.ac.ae>
2025-04-24 07:11:09 +0400

    Update index.js

--- 03/prod/index.js
+++ 03/prod/index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello, Azure!");
+    res.end("Hello Again!!");
 });
 
 const PORT = process.env.PORT || 8080;
