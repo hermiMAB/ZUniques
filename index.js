b390aea2fe298974c6af45dc1553e7fbf2cf34f2
Hermela Amha <hermelaamha.956@gmail.com>
2025-04-24 03:43:49 +0000

    A New Index Js Res fixed

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello, Azure!");
     res.end("Hello, Again!");
 });
 
